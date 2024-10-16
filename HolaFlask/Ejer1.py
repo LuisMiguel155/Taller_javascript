75308d6f18a821db6176ee67e0032dde9c832af3
LuisMiguel155 <luizmiguelc33@gmail.com>
2024-10-16 16:54:34 -0500

    lol

--- HolaFlask/Ejer1.py
+++ HolaFlask/Ejer1.py
@@ -1,29 +1,22 @@
-## Importamos flask
+## Impotamos flask
 from flask import Flask
 app=Flask(__name__)
 
-## Definimos la ruta principal
+## Defininmos la ruta principal
 @app.route("/")
 def HolaFlask():
     return "<h1>¡Hola Flask!</h1> <hr>"
 
-## Definimos una segunda ruta
-@app.route("/ruta2")
-def ruta2():
-    return "<strong> Estamos en la segunda ruta </strong> <hr>"
-
-## Definimos una tercera ruta
-@app.route("/ruta3")
-def ruta3():
-    return "<em> Estamos en la tercera ruta </em> <hr>"
-
+## Ahora tomamos la segunda ruta y la reemplazamos por el siguiente ejemplo
+## 1.) Haga un progama que calcule el promedio de notas sabiendo que tienen un valor de 
+## 30%, 30% y 40% respectivamente.
 @app.route("/notas")
 @app.route("/notas/<float:nota1>/<float:nota2>/<float:nota3>")
 def notas(nota1=0,nota2=0,nota3=0):
     resultado=(nota1*30)/100+(nota2*30)/100+(nota3*40)/100
     return f"<h1>El resultado es: {resultado}</h1> <hr>"
 
-if __name__=='__main__':
+if __name__=='main_':
     ## El valor True indica que la app se deja en modo debug
     app.run(debug=True)
     
